dd2b3ef7f566f86fcca325cfdfbbb7b7a2131c4d
Hugo Fernandes <hugo.fernandes@squadra.com.br>
2019-05-22 13:42:42 -0300

    Create schema.

--- api/src/index.js
+++ api/src/index.js
@@ -1,13 +1,38 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const graphHttp = require('express-graphql');
-const schema = require('./schema');
+// const schema = require('./schema');
+const { buildSchema } = require('graphql');
 
 const app = express();
+app.use(bodyParser.json());
 
-app.use('/graphql', graphHttp({
-    schema,
-    graphiql: true
-}));
+app.use('/graphql',
+    graphHttp({
+        schema: buildSchema(`
+        type RootQuery {
+            events: [String!]!
+        }
+        type RootMutation {
+            createEvent(name: String): String
+        }
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+        rootValue: {
+            events: () => {
+                return ['Romantic Cooking', 'Sailing', 'All-Night Coding'];
+            },
+            createEvent: (args) => {
+                const eventName = args.name;
+                return eventName;
+            }
+        },
+        graphiql: true
+    })
+);
 
 const PORT = process.env.PORT || 5000;
 
